2144a84b5ccb7f3271890ee0ef2d057f8ce8dda0
Kate <nemkovich.kate@gmail.com>
2022-08-31 23:09:03 +0300

    First

--- PluginApplication/Application.cs
+++ PluginApplication/Application.cs
@@ -1,6 +1,23 @@
-﻿namespace PluginApplication;
+﻿using System.Reflection;
+using Autodesk.Revit.UI;
 
-public class Application
+namespace PluginApplication;
+
+public class Application:IExternalApplication
 {
-    
+    public Result OnStartup(UIControlledApplication application)
+    {
+        application.CreateRibbonTab("PluginApplication");
+        var ribbonPanel = application.CreateRibbonPanel("PluginApplication","Panel");
+        var type = typeof (Command);
+        ribbonPanel.AddItem(new PushButtonData("Button", "Something", Assembly.GetAssembly(type).Location,
+            type.FullName));
+
+        return Result.Succeeded;
+    }
+
+    public Result OnShutdown(UIControlledApplication application)
+    {
+        return Result.Succeeded;
+    }
 }
--- PluginApplication/Class1.cs
+++ PluginApplication/Class1.cs
@@ -1,5 +1,14 @@
-﻿namespace PluginApplication;
-public class Class1
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace PluginApplication;
+
+public class Command: IExternalCommand
 {
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+        TaskDialog.Show("Hello", "It's working");
+        return Result.Succeeded; 
+    }
 
 }
