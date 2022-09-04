d843f67bd2a57d7ac334be2454515bb377b96381
Kate <nemkovich.kate@gmail.com>
2022-09-04 19:31:42 +0300

    Transaction

--- PluginApplication/Class1.cs
+++ PluginApplication/Class1.cs
@@ -1,8 +1,9 @@
-﻿using Autodesk.Revit.DB;
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
 namespace PluginApplication;
-
+[Transaction(TransactionMode.Manual)]
 public class Command: IExternalCommand
 {
     public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
