e07a1d57bb285f005209abfa6d88d7334c4a5af6
Kate <nemkovich.kate@gmail.com>
2022-09-05 21:44:20 +0300

    new version

--- PluginApplication/Class1.cs
+++ PluginApplication/Class1.cs
@@ -10,18 +10,20 @@ public class Command : IExternalCommand
 {
     public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
     {
-        var uiDocument = commandData.Application.ActiveUIDocument;
-        var element = uiDocument.Selection.PickObject(ObjectType.Element);
+        var uiDocument =
+            commandData.Application
+                .ActiveUIDocument; //блок ссылок на основные классы, которые будут использованы много раз
         var document = uiDocument.Document;
-        var receivedElement=document.GetElement(element);
-        var idelement = receivedElement.GetTypeId();
-        var element1 = document.GetElement(idelement);
-        TaskDialog.Show("Pick element", receivedElement.Name);
-        TaskDialog.Show("Category", receivedElement.Category.Name);
-        TaskDialog.Show("Size",element1.Name);
-        TaskDialog.Show("Size", ((ElementType)document.GetElement(idelement)).FamilyName);
-        
-        
+
+        var reference = uiDocument.Selection.PickObject(ObjectType.Element); //блок вычислений
+        var element = document.GetElement(reference);
+        var typeId = element.GetTypeId();
+        var type = (ElementType) document.GetElement(typeId);
+
+        TaskDialog.Show("Pick element", element.Name); //блок вывода на экран
+        TaskDialog.Show("Category", element.Category.Name);
+        TaskDialog.Show("Size", type.Name);
+        TaskDialog.Show("Size", type.FamilyName);
 
         return Result.Succeeded;
     }
